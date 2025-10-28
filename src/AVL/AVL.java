704a106511e779a7539578e029d388bce8db8a2d
Himanshux07 <annikumar997@gmail.com>
2025-10-28 23:24:30 +0530

    AVL

--- src/AVL/AVL.java
+++ src/AVL/AVL.java
@@ -71,6 +71,7 @@ class AVL {
     }
 
     if (height(node.left) - height(node.right) < -1) {
+
       // right heavy
       if(height(node.right.left) - height(node.right.right) < 0) {
         // right right case
